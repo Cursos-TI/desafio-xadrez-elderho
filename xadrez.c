5d6443324cdd98936fec3740c561a859a05b1b3b
Elder Henrique de Oliveira <151265145+elderho@users.noreply.github.com>
2025-10-22 23:30:12 -0300

    Update xadrez.c
    Movimentação de peças de xadrez nível novato.

--- xadrez.c
+++ xadrez.c
@@ -28,5 +28,49 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
+    // ###########################################################
+    // TORRE - movimenta-se em linha reta (horizontal ou vertical)
+    // ###########################################################
+    int it; 
+    int movtorre = 5; // número de casas que a torre vai andar
+    
+    printf(">>>>> Movimento da TORRE: %d casas <<<<<\n", movtorre);
+    // A torre se move em linha reta, 5 casas para a direita
+    for (it = 1; it <= movtorre; it++) {
+        printf("Direita (%d)\n", it);
+    }
+
+    // ###########################################################
+    // BISPO - movimenta-se em diagonais
+    // ###########################################################
+    int ib = 1;
+    int movbispo = 5; // número de casas que o bispo vai andar
+
+    printf("\n>>>>> Movimento do BISPO: %d casas <<<<<\n", movbispo);
+    // O bispo se move na diagonal, para cima e para a direita
+    while (ib <= movbispo) {
+        printf("Cima, Direita (%d)\n", ib);
+        ib++;
+    }
+
+    // ###########################################################
+    // RAINHA - movimenta-se em qualquer direção
+    // ###########################################################
+    int ir = 1;
+    int movrainha = 8; // número de casas que a rainha vai andar
+
+    printf("\n>>>>> Movimento da RAINHA: %d casas <<<<<\n", movrainha);
+    // A rainha se move em todas as direções, neste caso será para a esquerda
+    do {
+        printf("Esquerda (%d)\n", ir);
+        ir++;
+    } while (ir <= movrainha);
+
+    // ###########################################################
+    // Indicação de final do programa
+    // ###########################################################
+    printf("\n>>>>> MOVIMENTAÇÃO FINALIZADA! <<<<<\n");
+
+
     return 0;
 }
